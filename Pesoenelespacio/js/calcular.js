936b917fdca04d598ed680f4acabd07f3230d764
Miguel porras <porraangel@hotmail.com>
2019-10-20 23:53:38 -0500

    empezado trabajar con cannvas

--- Pesoenelespacio/js/calcular.js
+++ Pesoenelespacio/js/calcular.js
@@ -1,1 +1,25 @@
-alert("hola")
+
+function validar(){
+
+ 
+var usuario = document.getElementById("usuario").value;
+
+
+ var peso = parseInt(usuario);
+
+ var Tierra = 9.8;
+ var Marte =  3.7;
+
+
+
+var  PesoFinal  = 0 ;
+
+
+PesoFinal = peso * Marte / Tierra;
+ 
+ document.getElementById("hello").innerHTML ="tu peso es marte es el siguente:  "+ PesoFinal;
+
+
+ }
+
+
