5f7b9f4c1c5212b7e05cbf0bd52bf6516576140d
Miguel porras <porraangel@hotmail.com>
2019-11-04 22:25:54 -0500

    terminado el paquiman

--- paquiman/js/Clases.js
+++ paquiman/js/Clases.js
@@ -1,1 +1,47 @@
-alert("corre perra");
+//alert("corre perra");
+
+var foto = [];
+foto["Pollote"]= "pollo.png";
+foto["Tocinauro"]= "cerdo.png";
+foto["Vacota"]= "vaca.png";
+
+
+class paquiman {
+
+
+  constructor (n,v,a)
+  {
+  	this.imagen = new Image();
+  	this.nombre = n;
+  	this.vida = v;
+  	this.ataque = a;
+  	this.imagen.src = foto[this.nombre];
+  }
+
+  mostrar(){
+  	document.body.appendChild(this.imagen)
+  	document.write("<br>"+this.nombre + "<br>")
+  	document.write(this.vida + "<br>")
+  	document.write(this.ataque + "<br>")
+  
+  }
+
+}
+
+
+var Pollote = new paquiman("Pollote", 100 ,80) ;
+var Tocinauro = new paquiman("Tocinauro",120 , 60);
+var Vacota = new paquiman("Vacota",120 , 60);
+
+coleccion = []
+{
+	coleccion.push(new paquiman("Pollote", 100 ,80)); 
+    coleccion.push(new paquiman("Tocinauro",120 , 60));
+     coleccion.push(new paquiman("Vacota",120 , 60));
+
+}
+
+for(h  of coleccion){
+
+	h.mostrar();
+}
