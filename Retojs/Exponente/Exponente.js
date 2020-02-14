c2ed7374eb6f0145f3d445640dda40f7b5a0f292
miguel porras <porraangel@gmail.com>
2020-02-14 18:59:13 -0500

    ordenamiento

---
+++ Retojs/Exponente/Exponente.js
@@ -0,0 +1,26 @@
+
+
+
+function  Recursividad(Num){
+
+
+    var Exp =1;
+	console.log(Num)
+	if(Num = 0){
+		return Exp
+
+       }
+
+
+   Exp = Exp * Recursividad(Num -1);
+	console.log(Exp);
+	return Exp;
+
+	
+}
+
+function contenidoid2(id) {
+	console.log(id)
+    
+
+}
